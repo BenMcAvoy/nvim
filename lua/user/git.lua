c6bfd8c2fcd3f3931027f2cc5dcf0e041f865cad
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-05-08 13:29:45 +0100

    feat: configure git

--- lua/user/git.lua
+++ lua/user/git.lua
@@ -16,19 +16,19 @@ require('gitsigns').setup {
 	},
 	auto_attach = true,
 	attach_to_untracked = false,
-	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	current_line_blame_opts = {
 		virt_text = true,
 		virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-		delay = 1000,
+		delay = 250,
 		ignore_whitespace = false,
-		virt_text_priority = 100,
+		virt_text_priority = 0,
 	},
 	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
 	current_line_blame_formatter_opts = {
 		relative_time = false,
 	},
-	sign_priority = 6,
+	sign_priority = 20,
 	update_debounce = 100,
 	status_formatter = nil, -- Use default
 	max_file_length = 40000, -- Disable if file is longer than this (in lines)
