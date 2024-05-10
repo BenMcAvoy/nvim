6857b439e29240e3787b4ff4a68b3f9f8b49cbdc
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-05-10 11:05:57 +0100

    remove whitespace implementation

--- lua/user/whitespace.lua
+++
@@ -1,7 +0,0 @@
-vim.g.show_spaces_that_precede_tabs = 1
-vim.g.better_whitespace_enabled = 1
-vim.g.strip_whitespace_on_save = 1
-
-vim.keymap.set("n", "<Leader>t", vim.cmd.StripWhitespace)
-
-vim.cmd.DisableWhitespace()
